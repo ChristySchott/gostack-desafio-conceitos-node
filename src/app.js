d79106d2900d133e1b654ae811dc4f917d995ca7
Christy Hauschild Schott <christyhauschild@gmail.com>
2020-08-18 00:16:25 -0300

    feat: add like to repository

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  const { id } = response.params;
+  const { id } = request.params;
   const { title, url, techs } = request.body;
 
   const repositoryIndex = repositories
@@ -39,7 +39,8 @@ app.put("/repositories/:id", (request, response) => {
     id,
     title,
     url,
-    techs
+    techs,
+    likes: 0
   };
 
   repositories[repositoryIndex] = repository;
@@ -64,7 +65,27 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories
+    .findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found!' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes + 1
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository)
 });
 
 module.exports = app;
